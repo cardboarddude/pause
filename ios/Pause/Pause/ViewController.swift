c086d88ce36d7b7f6989a01ff82ec818b976edc2
Thomas Elliott <tom.w.elliott@gmail.com>
2016-04-17 12:11:44 -0400

    Add template iOS project.

---
+++ ios/Pause/Pause/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pause
+//
+//  Created by Thomas Elliott on 4/17/16.
+//  Copyright © 2016 Pause. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
